c11831ead4337af5f4c0ebeff91b06cc42dded86
Thomas <thomasshaw258@gmail.com>
2023-03-06 20:59:40 -0800

    Initial commit.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.chess {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.chess to javafx.fxml;
+    exports com.example.chess;
+}
