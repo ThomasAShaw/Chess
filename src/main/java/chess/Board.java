1481529176ce1954189db354aa4da7e903d96c1a
masood4y <masood4y@gmail.com>
2023-03-07 17:12:42 -0800

    constructor half-way done

--- src/main/java/chess/Board.java
+++ src/main/java/chess/Board.java
@@ -9,6 +9,27 @@ public class Board {
     List<Piece> listOfDeadPieces = new ArrayList<>();
 
 
+    public Board(){
+
+        for(int i = 0; i < 8; i++){
+            Piece blackPawn = new Piece(false, PieceType.PAWN);
+            board[1][i] = blackPawn;
+        }
+
+        for(int i = 0; i < 8; i++){
+            for(int j = 2; j <= 5; j++ ){
+                Piece NullPiece = new Piece(false, PieceType.PAWN);
+                board[j][i] = NullPiece;
+            }
+        }
+
+
+
+    }
+
+
+
+
 
 
 
